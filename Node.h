76760cf696b7b13d20038702767c48503cec5ec0
TuNombre <TuEmail>
2025-01-09 01:32:59 +0100

    nodesito

---
+++ Node.h
@@ -0,0 +1,30 @@
+#include <ostream>
+
+template <typename T> 
+class Node {
+    public:
+        T data;
+	Node<T>* next;
+
+        Node(T data, Node<T>* next=nullptr){
+	
+            this->data = data;
+	    this->next = next;
+	
+	}
+        friend std::ostream& operator<<(std::ostream &out, const Node<T> &node){
+	
+	out<<node.data<<std::endl;
+	return out;
+	
+	
+	
+	
+	}
+
+
+
+
+};
+
+
